1d4032686a86f18332c775f918411d67a3fefb45
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-02-27 19:19:34 +0600

    s

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5175;
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://helaluddin-swe-destinationjobs.vercel.app/", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
