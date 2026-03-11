67ec93f75868fe7dcd0d7ad22bed75cd8146b141
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-03-12 05:54:43 +0600

    s

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["", "http://localhost:5173"],
+  origin: ["https://jobspace-client.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
